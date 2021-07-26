196a3864781887cb4a2011107f8089a0a4ea6a52
tarrinneal <tarrinneal@gmail.com>
2021-07-25 17:46:34 -0700

    basic init started and basic docs

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # discordbattlebots
+
+#Bot setup
+
+The ref bot is startes with a `!bb init` command.
+
+In response it expects a `!bb @<user>`
+
+the ref bot will then tag the battle bot with a `@<user> init`
+
+In response the ref bot expects a stringified JSON obbject containing the battle bots stats
--- index.js
+++ index.js
@@ -1,14 +1,63 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+require('dotenv').config();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', (msg) => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+  let sent = false;
+  if (msg.content.startsWith('!bb')) {
+    if (msg.content === '!bb') {
+      msg.channel.send(
+        'Check out the readme for instructions on how to play at https://github.com/tarrinneal/discordbattlebots'
+      );
+      sent = true;
+    }
+    if (msg.content === '!bb init') {
+      gameState.init = true;
+      msg.reply('Initializing game, tag your champions!');
+      sent = true;
+    }
+    if (msg.content.includes('!bb <@') && gameState.init) {
+      if (gameState.player1 === '') {
+        gameState.player1 = msg.content.split('!bb ')[1];
+        msg.reply(`${gameState.player1} !init`);
+        sent = true;
+      } else if (gameState.player2 === '') {
+        gameState.player2 = msg.content.split('!bb ')[1];
+        msg.reply(`${gameState.player2} !init`);
+        sent = true;
+      }
+    }
+    if (
+      gameState.player1.includes(msg.author.id) &&
+      msg.content.includes('!bb {')
+    ) {
+      gameState.player1Stats = JSON.parse(msg.content.split('!bb ')[1]);
+      msg.reply(JSON.stringify(gameState.player1Stats));
+      sent = true;
+    } else if (
+      gameState.player2.includes(msg.author.id) &&
+      msg.content.includes('!bb {')
+    ) {
+      gameState.player2Stats = JSON.parse(msg.content.split('!bb ')[1]);
+      msg.reply(JSON.stringify(gameState.player1Stats));
+      sent = true;
+    }
+    if (!sent) {
+      msg.reply('Invalid command!');
+    }
   }
 });
 
-client.login('token');
+client.login(process.env.distok);
+
+const gameState = {
+  init: false,
+  player1: '',
+  player2: '',
+  player1Stats: undefined,
+  player2Stats: undefined,
+};
